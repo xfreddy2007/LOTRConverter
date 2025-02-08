1c698ce4ef1d81f0f4f9af8a8bce82bd8d953e3f
freddyliu2024 <160204550+freddyliu2024@users.noreply.github.com>
2025-02-08 13:20:46 +0800

    feat: add content view layout

--- LOTRCOnverter/ContentView.swift
+++ LOTRCOnverter/ContentView.swift
@@ -1,22 +1,105 @@
-//
-//  ContentView.swift
-//  LOTRCOnverter
-//
-//  Created by 劉宣甫 on 2025/2/8.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State var showExcahngeInfo = false
+  @State var leftAmount = ""
+  @State var rightAmount = ""
+  
+  var body: some View {
+    ZStack {
+      // Background image
+      Image(.background)
+        .resizable()
+        .ignoresSafeArea()
+      
+      VStack {
+        // Prancing pony image
+        Image(.prancingpony)
+          .resizable()
+          .scaledToFit()
+          .frame(height: 200)
+        
+        // Currency Exchange text
+        Text("Currency Exchange")
+          .font(.largeTitle)
+          .foregroundStyle(.white)
+        
+        // Conversion section
+        HStack {
+          // Left convertion section
+          VStack {
+            // Currency
+            HStack {
+              // Currency image
+              Image(.silverpiece)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+              
+              // Currency text
+              Text("Siver Piece")
+                .font(.headline)
+                .foregroundStyle(.white)
+            }
+            .padding(.bottom, -5)
+            
+            // Text field
+            TextField("Amount", text: $leftAmount)
+              .textFieldStyle(.roundedBorder)
+          }
+          
+          // Equal sign
+          Image(systemName: "equal")
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .symbolEffect(.pulse)
+          
+          // Right conversion section
+          VStack {
+            // Currency
+            HStack {
+              // Currency text
+              Text("Gold Piece")
+                .font(.headline)
+                .foregroundStyle(.white)
+              
+              // Currency image
+              Image(.goldpiece)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            }
+            .padding(.bottom, -5)
+            
+            // Text field
+            TextField("Amount", text: $rightAmount)
+              .textFieldStyle(.roundedBorder)
+              .multilineTextAlignment(.trailing)
+            
+          }
         }
         .padding()
+        .background(.black.opacity(0.5))
+        .clipShape(.capsule)
+        
+        Spacer()
+        
+        // info button
+        HStack {
+          Spacer()
+          Button {
+            showExcahngeInfo.toggle()
+            print("showExcahngeInfo value: \(showExcahngeInfo)")
+          } label: {
+            Image(systemName: "info.circle.fill")
+              .font(.largeTitle)
+              .foregroundStyle(.white)
+          }
+          .padding(.trailing)
+        }
+      }
     }
+  }
 }
 
 #Preview {
