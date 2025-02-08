df65a2f00d189f71fb292f8611495c2425ba5e47
freddyliu2024 <160204550+freddyliu2024@users.noreply.github.com>
2025-02-08 10:21:44 +0800

    Initial Commit

---
+++ LOTRCOnverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRCOnverter
+//
+//  Created by 劉宣甫 on 2025/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRCOnverter/LOTRCOnverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRCOnverterApp.swift
+//  LOTRCOnverter
+//
+//  Created by 劉宣甫 on 2025/2/8.
+//
+
+import SwiftUI
+
+@main
+struct LOTRCOnverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
